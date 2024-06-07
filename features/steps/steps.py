fcf0f01a634eee0b924afc958e2986cdab81ed36
rbi-aap <rbi@aap.com.au>
2024-06-07 16:31:59 +1000

    update

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -19,6 +19,7 @@ from wooper.general import (
 )
 import logging
 
+
 @when('we save API token')
 def step_save_token(context):
     context.headers.append(('Authorization', context.news_api_tokens.get('_id')))
@@ -63,6 +64,7 @@ def we_set_api_time_limit(context, value):
     with context.app.test_request_context():
         get_settings_collection().insert_one({"_id": "general_settings", "values": {"news_api_time_limit_days": value}})
 
+
 @then('we "{get}" "{text}" in atom xml response')
 def we_get_text_in_atom_xml_response(context, get, text):
     with context.app.test_request_context(context.app.config['URL_PREFIX']):
@@ -73,6 +75,7 @@ def we_get_text_in_atom_xml_response(context, get, text):
             assert (text in get_body(context.response))
         else:
             assert (text not in get_body(context.response))
+
 
 @then('we "{get}" "{text}" in syndicate xml response')
 def we_get_text_in_syndicate_xml_response(context, get, text):
