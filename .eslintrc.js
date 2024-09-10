f1ee3c55568066472012b7612eb782d64955c0c6
rbi-aap <rbi@aap.com.au>
2024-09-10 15:11:28 +1000

    for update the ESLint  for webpack and React

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,45 +10,39 @@ module.exports = {
     "extends": ["eslint:recommended", "plugin:react/recommended"],
     "parserOptions": {
         "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
             "jsx": true
         },
+        "ecmaVersion": 2018,
         "sourceType": "module"
     },
     "plugins": [
         "react"
     ],
     "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-console": [
-            "error",
-            {"allow": ["warn", "error"]}
-        ],
-        "react/no-deprecated": [
-            1,
-        ],
-        "react/jsx-no-target-blank": [
-            0,
-        ],
+        "indent": ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["error", "single"],
+        "semi": ["error", "always"],
+        "no-console": ["error", {"allow": ["warn", "error"]}],
+        "react/no-deprecated": [1],
+        "react/jsx-no-target-blank": [0]
     },
     "settings": {
         "react": {
             "version": "16.2"
         }
-    }
+    },
+    "overrides": [
+        {
+            "files": ["webpack.config.js", "*.config.js"],
+            "env": {
+                "node": true,
+                "commonjs": true
+            },
+            "globals": {
+                "__dirname": "readonly",
+                "process": "readonly"
+            }
+        }
+    ]
 };
